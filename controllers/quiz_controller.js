bb3a6a992536bcf028f789f09911e8903637fe41
JavierAmor <javieramordelacruz@gmail.com>
2016-04-24 10:04:45 +0000

    No perder respuesta al reintentar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,10 +1,12 @@
 // GET /question
 exports.question = function(req, res, next){
-res.render('quizzes/question', {question: '¿Cual es la capital de Italia?'});
+var answer = req.query.answer || '';
+res.render('quizzes/question', {question: '¿Cual es la capital de Italia?', answer : answer});
 };
 
 // GET /check
 exports.check = function(req, res, next){
+var answer = req.query.answer || '';
 var result= req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-res.render('quizzes/result', {result: result});
+res.render('quizzes/result', {result : result, answer : answer});
 };
