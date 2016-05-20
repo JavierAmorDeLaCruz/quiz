451bc8849c58a8cf1d6353d968937d5d4fb0ad89
Javier <javier.amor.delacruz@alumnos.upm.es>
2016-05-20 17:23:42 +0200

    Tema 20: Gestión de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,8 +16,18 @@ router.get('/', function(req, res, next) {
 
 // Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('userId', userController.load); // autoload :userId
 
+// Definición de rutas de cuenta
+router.get('/users',                    userController.index);   // listado usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign un
+router.post('/users',                   userController.create);  // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);     // editar información de cuenta
+router.put('/users/:userId(\\d+)',      userController.update);   // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)',   userController.destroy);  // borrar cuenta
 
+//quizes
 router.get('/quizzes.:format?', quizController.index);
 router.get('/quizzes/:quizId(\\d+).:format?', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
@@ -29,6 +40,7 @@ router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
+//Comments
 router.get('/quizzes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',     commentController.create);
 
